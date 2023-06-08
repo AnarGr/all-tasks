b83422c5bbcaa3c6d3526027717b4b7c6a0be497
AnarGr <anarig@code.edu.az>
2023-06-08 16:21:14 +0400

    First add

---
+++ task1/ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int num1 = 2;
+        int num2 = 17;   
+        int num3 = 13;
+
+        int max = FindMax(num1, num2, num3);
+        Console.WriteLine(max);
+    }
+
+    static int FindMax(int a, int b, int c)
+    {
+        int max = a;
+
+        if (b > max)
+            max = b;
+
+        if (c > max)
+            max = c;
+
+        return max;
+    }
+}
---
+++ task2/tsk3/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int num = 173;
+        int count = 0;
+
+        for (int i = num; i > 0; i /= 10)
+        {
+            count++;
+        }
+
+
+        Console.WriteLine($"Daxil edilmiş ədəd {count} mərtəbəlidir");
+    }
+}
