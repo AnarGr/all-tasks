42417da486b7be0881f41eae31da2760445e7d41
AnarGr <135498443+AnarGr@users.noreply.github.com>
2023-06-13 16:41:59 +0400

    Add files via upload

---
+++ L2.Task1/L2.Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using System;
+
+namespace L2.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mashin mashin1 = new Mashin();
+
+            mashin1.Brand = "BMW";
+            mashin1.Model = "M5";
+            mashin1.CurrentFuel = 70.0;
+            mashin1.FuelFor1Km = 0.3;
+            mashin1.Millage = 17588;
+
+            mashin1.Drive(220);
+            Console.WriteLine("Yol: {0} km", mashin1.Millage);
+            Console.WriteLine("Benzin: {0} litr", mashin1.CurrentFuel);
+        }
+    }
+}
